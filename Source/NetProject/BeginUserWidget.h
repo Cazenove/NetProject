6520076a91c4ee1e8f1fd32b376784b05677d6df
Cazenove <707533554@qq.com>
2020-10-29 20:25:37 +0800

    简易连接

---
+++ Source/NetProject/BeginUserWidget.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/CanvasPanel.h"
+#include "Components/TextBlock.h"
+#include "Components/ScrollBox.h"
+#include "BeginUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETPROJECT_API UBeginUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	/*
+private:
+	virtual bool Initialize() override;
+
+public:
+	UPROPERTY()
+		UCanvasPanel* BeginPanel;
+	UPROPERTY()
+		UButton* CreateButton;
+	UPROPERTY()
+		UButton* JoinButton;
+	UPROPERTY()
+		UButton* QuitButton;
+	UPROPERTY()
+		UTextBlock* CreatText;
+	UPROPERTY()
+		UTextBlock* JoinText;
+	UPROPERTY()
+		UTextBlock* QuitText;
+
+public:
+	UFUNCTION()
+		void CreateSession();
+	UFUNCTION()
+		void JoinSession();
+	UFUNCTION()
+		void QuitGame();*/
+};
